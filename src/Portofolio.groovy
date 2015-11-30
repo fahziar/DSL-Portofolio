e33569a820027f6e0ec418f1836ffd6049111ee7
fahziar <fahziar_rw@yahoo.co.uk>
2015-11-30 15:31:20 +0700

    Portofolio

--- src/Portofolio.groovy
+++ src/Portofolio.groovy
@@ -2,18 +2,48 @@
  * Created by fahziar on 30/11/2015.
  */
 class Portofolio {
-    String nama;
+    String namaPemilik;
     String deskripsi;
     String foto;
-    String email;
-    Keahlian[] keahlian;
+    Kontak kontakPemilik;
+    List<Keahlian> daftarKeahlian;
+    List<Pekerjaan> daftarPekerjaan;
 
+    def static make(Closure buatPortofolio){
+        Portofolio portofolio = new Portofolio();
+        buatPortofolio.delegate = portofolio;
+        detilPortofolio()
+        println buatPortofolio.delegate;
+    }
 
+    def nama(String namaPemilik){
+        this.namaPemilik = namaPemilik;
+    }
 
-    def static make(Closure closure){
-        closure.delegate = new Portofolio(                         );
-        closure()
-        println closure.delegate;
+    def deskripsiDiri(String deskripsi){
+        this.deskripsi = deskripsi;
+    }
+
+    def linkFoto(String foto){
+        this.foto = foto;
+    }
+
+    def kontak(Kontak kontakPemilik){
+        this.kontakPemilik = kontakPemilik;
+    }
+
+    def keahlian(Closure tambahKeahlian){
+        Keahlian tmp = new Keahlian();
+        tambahKeahlian.delegate = tmp;
+        tambahKeahlian()
+        this.daftarKeahlian.push(tmp);
+    }
+
+    def pekerjaan(Closure tambahPekerjaan){
+        pekerjaan tmp = new Pekerjaan();
+        tambahPekerjaan.delegate = tmp;
+        tambahPekerjaan();
+        this.daftarPekerjaan.push(tmp);
 
     }
 }
